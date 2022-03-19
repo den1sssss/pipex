4fddfa9ed9747a9e0871ae711f2dad9b87ca15d2
Dontos Shirely <dshirely@il-c4.msk.21-school.ru>
2022-03-19 13:18:53 +0300

    17/03

---
+++ pipex.c
@@ -0,0 +1,13 @@
+#include "pipex.h"
+//pipe()
+//fork()
+//dub2()
+//execve()
+int main(int argc, char **argv)
+{
+	if(argc == 5)
+	{
+
+	}
+	return(0);
+}
---
+++ pipex.h
@@ -0,0 +1,11 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <unistd.h>
+#include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+# include <sys/wait.h>
+# include <stdio.h>
+#endif
